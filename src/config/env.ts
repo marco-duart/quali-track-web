83cc9a112bf4350899a0c39110a8ae7a2038ec19
marco-duart <marcoaurelio_db@hotmail.com>
2024-09-22 10:15:07 -0300

    feat: added firsts files

--- src/config/env.ts
+++
@@ -1,7 +0,0 @@
-
-const env = {
-  BASE_URL: import.meta.env.VITE_BASE_URL as string,
-  API_KEY: import.meta.env.VITE_API_KEY as string,
-};
-
-export { env };
--- src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { env } from "../config/env";
-
-export default axios.create({
-  baseURL: env.BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
