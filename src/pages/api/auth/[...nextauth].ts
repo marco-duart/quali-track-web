a6196a5593e3d70f7a73ee5f44328273bf86cd9e
marco-duart <marcoaurelio_db@hotmail.com>
2024-09-22 11:09:41 -0300

    feat: added next-auth dependency

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,48 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import axios from 'axios';
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+      authorize: async (credentials) => {
+        try {
+          const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+            email: credentials?.email,
+            password: credentials?.password,
+          }, { withCredentials: true });
+
+          if (response.data) {
+            return response.data;
+          }
+          return null;
+        } catch (error) {
+          return null;
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/auth/login',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.role = token.role as string;
+      }
+      return session;
+    },
+  },
+});
