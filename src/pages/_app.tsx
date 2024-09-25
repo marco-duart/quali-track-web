f2f53e3b21375ce89ae4d03ac4afc90606db5613
marco-duart <marcoaurelio_db@hotmail.com>
2024-09-25 11:14:38 -0300

    feat: added redirect on startup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,19 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import theme from '../styles/theme';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { SessionProvider } from "next-auth/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../styles/theme";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
-function MyApp({ Component, pageProps }: any) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </QueryClientProvider>
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </QueryClientProvider>
+    </SessionProvider>
   );
 }
 
